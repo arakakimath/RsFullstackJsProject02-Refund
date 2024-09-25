85892a43b80b40889c3aac58a7ba043244f65c75
Matheus Arakaki <arakakimath@gmail.com>
2024-09-25 07:12:24 -0400

    permite só números no input e formata para BRL

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Js connected")
+// Seleciona os elementos do formulário
+const amount = document.getElementById("amount");
+
+// Captura o evento de input para formatar o valor.
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "");
+
+  // Transforma o valor em centavos (exemplo: 150/100 = 1.5)
+  value = Number(value)/100;
+
+  amount.value = formatCurrencyBRL(value);
+}
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return value;
+}
