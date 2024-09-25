667f330b90dac11d6bc4fbf2f4dac0840c010503
Matheus Arakaki <arakakimath@gmail.com>
2024-09-25 07:21:20 -0400

    submit creates objects

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
@@ -19,4 +22,19 @@ function formatCurrencyBRL(value) {
   });
 
   return value;
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+
+  console.log(newExpense)
 }
